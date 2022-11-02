1731787093eeb4d4daa585ab7fa09ba7c923fb28
1sarah <mbuyaeugenis@gmail.com>
2022-11-02 17:28:22 +0300

    security confuguration

--- lib/do_it_web/graphql/queries/user.ex
+++ lib/do_it_web/graphql/queries/user.ex
@@ -1,3 +1,19 @@
 defmodule DoItWeb.Graphql.Queries.User do
   use Absinthe.Schema.Notation
+
+  alias DoIt.Accounts.Accounts
+
+  def get_all_users(_parent, args, context) do
+    with {:auth, %{context: %{current_user: _user}}} <- {:auth, context},
+         {:users, users} <- {:users, Accounts.list_all_users(args)} do
+
+      {:ok, %{users: users, total: length(Accounts.list_users()), success: true}}
+    else
+      {:auth, _} ->
+        {:error, "You must be authenticated!"}
+
+      {:users, _} ->
+        {:error, "Empty Record"}
+    end
+  end
 end
